8e60754d63e124417cad66e0f335c172834c1c2a
Redskull-127 <meerraja17@gmail.com>
2023-01-16 18:45:49 +0530

    Done and Dusted

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["images.pexels.com", "player.vimeo.com"],
+  },
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  basePath: "",
+  assetPrefix: "",
 }
 
 module.exports = nextConfig
