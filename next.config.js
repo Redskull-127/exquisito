64fc915a5bcd2eed618ab22e8d17dd342c34ee1a
Redskull-127 <meerraja17@gmail.com>
2023-01-16 19:14:01 +0530

    pom pom

--- next.config.js
+++ next.config.js
@@ -3,8 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["images.pexels.com", "player.vimeo.com"],
+    unoptimized: true,
   },
-
+  
 }
 
 module.exports = nextConfig
